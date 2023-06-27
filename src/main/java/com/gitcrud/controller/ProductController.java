9ba622771f710ce39ee05e67fa1b6da293935731
tuan_anh <119550348+tuananhafjdjhv@users.noreply.github.com>
2023-06-27 15:18:26 +0700

    create product

--- src/main/java/com/gitcrud/controller/ProductController.java
+++ src/main/java/com/gitcrud/controller/ProductController.java
@@ -1,12 +1,26 @@
 package com.gitcrud.controller;
 
+import com.gitcrud.model.Product;
+import com.gitcrud.service.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/product")
+@RequiredArgsConstructor
 public class ProductController {
 
+    private final IProductService productService;
+    @PostMapping("/create")
+    public ResponseEntity<String> createProduct(@RequestBody Product product){
+        productService.save(product);
+        return ResponseEntity.ok().body("Create success");
+    }
 
 }
--- src/main/java/com/gitcrud/service/serviceIMPL/ProductServiceIMPL.java
+++ src/main/java/com/gitcrud/service/serviceIMPL/ProductServiceIMPL.java
@@ -1,13 +1,18 @@
 package com.gitcrud.service.serviceIMPL;
 
 import com.gitcrud.model.Product;
+import com.gitcrud.repository.IProductRepository;
 import com.gitcrud.service.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceIMPL implements IProductService {
+    private final IProductRepository productRepository;
     @Override
     public List<Product> findAll() {
         return null;
@@ -20,7 +25,7 @@ public class ProductServiceIMPL implements IProductService {
 
     @Override
     public Product save(Product product) {
-        return null;
+        return productRepository.save(product);
     }
 
     @Override
