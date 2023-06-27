f3107af674cc76e526fbdd350fef2c2bcace12ea
khoamak246 <79485649+khoamak246@users.noreply.github.com>
2023-06-27 15:47:06 +0700

    Update ProductController.java

--- src/main/java/com/gitcrud/controller/ProductController.java
+++ src/main/java/com/gitcrud/controller/ProductController.java
@@ -22,44 +22,5 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductController {
 
-    private final IProductService productService;
-    @PostMapping("/create")
-    public ResponseEntity<String> createProduct(@RequestBody Product product){
-        productService.save(product);
-        return ResponseEntity.ok().body("Create success");
-    }
-
-    @GetMapping("")
-    public List<Product> findAllProduct(){
-        return productService.findAll();
-    }
-    @Autowired
-    private ProductServiceIMPL productServiceIMPL;
-    @GetMapping("/findByName")
-    public ResponseEntity<List<Product>> searchByName(@RequestParam("name") String name) {
-        List<Product> list = (List<Product>) productServiceIMPL.findProductByName(name);
-        if (list.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } else {
-            return new ResponseEntity<>(list, HttpStatus.OK);
-        }
-    }
-
-    @PutMapping("/edit/{id}")
-    public ResponseEntity<Product> editProduct(@PathVariable Long id, @RequestBody Product product) {
-        Product pro = productService.findById(id);
-        if (pro == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        product.setId(pro.getId());
-        return new ResponseEntity<>(productService.update(product), HttpStatus.OK);
-    }
-
-    @GetMapping("/findById/{id}")
-    private ResponseEntity<Product>findProductById(@PathVariable("id")Long id){
-        if (productService.findById(id)!=null){
-            return new ResponseEntity<>(productService.findById(id), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
+   
 }
