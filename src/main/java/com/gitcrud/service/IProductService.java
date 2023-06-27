aaa73b8b3af427e6823778984139653c12f9e71b
AnhKhoa <khoa.mak246@gmail.com>
2023-06-27 14:36:15 +0700

    update feature

--- src/main/java/com/gitcrud/service/IProductService.java
+++ src/main/java/com/gitcrud/service/IProductService.java
@@ -1,7 +1,9 @@
 package com.gitcrud.service;
 
+import com.gitcrud.model.Product;
+import com.gitcrud.service.design.IGeneric;
 import org.springframework.stereotype.Service;
 
-@Service
-public interface IProductService {
+
+public interface IProductService extends IGeneric<Product> {
 }
---
+++ src/main/java/com/gitcrud/service/design/IGeneric.java
@@ -0,0 +1,18 @@
+package com.gitcrud.service.design;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface IGeneric <T>{
+
+    List<T> findAll();
+    T findById(Long id);
+    T save(T t);
+    void deleteById(Long id);
+    T searchByName(String name);
+    T update(T t);
+
+
+}
